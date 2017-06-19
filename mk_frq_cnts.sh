9c3ec563ac5f6ed52cc83d2feb11d3c9dc059d86
tcatapano <thc4ster@gmail.com>
2017-06-19 17:12:54 +0200

    fixed freq count script and moved output

---
+++ mk_frq_cnts.sh
@@ -0,0 +1,18 @@
+#! //bin/bash
+
+ELEMENTS="al bp cn env m md ms mu pa pl pn pro sn tl tmp"
+
+rm term_usage/term_cnts.txt 
+
+for ELEMENT in $ELEMENTS
+
+do 
+XPATH="//$ELEMENT"
+echo $ELEMENT
+# xml sel -t -m '//cn' -n  -v .  *.xml | sort | uniq -c | sort -nr
+xml sel -t -m "$XPATH" -n -v .  tc/*.xml | sort | uniq -c | sort -nr > term_usage/"$ELEMENT"_fq.csv 
+
+wc -l term_usage/"$ELEMENT"_fq.csv >> term_usage/term_cnts.txt
+
+done
+
