a521716787ce97433178e55af525be2a3f3ca8c4
tcatapano <thc4ster@gmail.com>
2017-06-20 23:18:50 +0200

    download new xml; modify term count scrip; regnerate term counts

--- mk_frq_cnts.sh
+++ mk_frq_cnts.sh
@@ -10,7 +10,7 @@ do
 XPATH="//$ELEMENT"
 echo $ELEMENT
 # xml sel -t -m '//cn' -n  -v .  *.xml | sort | uniq -c | sort -nr
-xml sel -t -m "$XPATH" -n -v .  tc/*.xml | sort | uniq -c | sort -nr > term_usage/"$ELEMENT"_fq.csv 
+xml sel -t -m "$XPATH" -n -v .  tc/*.xml | sort | uniq -c | sort -nr | awk '{ print $2 "," $1}' > term_usage/"$ELEMENT"_fq.csv 
 
 wc -l term_usage/"$ELEMENT"_fq.csv >> term_usage/term_cnts.txt
 
