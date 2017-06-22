893b1bee2f2a81ae12a15b87a9ebbbae6221f31d
tcatapano <thc4ster@gmail.com>
2017-06-22 11:47:02 +0200

    revise term count script; rerun

--- mk_frq_cnts.sh
+++ mk_frq_cnts.sh
@@ -10,8 +10,7 @@ do
 XPATH="//$ELEMENT"
 echo $ELEMENT
 # xml sel -t -m '//cn' -n  -v .  *.xml | sort | uniq -c | sort -nr
-xml sel -t -m "$XPATH" -n -v .  tc/*.xml | sort | uniq -c | sort -nr | tr -s ' ' ' ' | cut -d' ' -f 2- | sed 's/ /|/1'
- > term_usage/"$ELEMENT"_fq.csv 
+xml sel -t -m "$XPATH" -n -v .  tc/*.xml | sort | uniq -c | sort -nr | tr -s ' ' ' ' | cut -d' ' -f 2- | sed 's/ /|/1' > term_usage/"$ELEMENT"_fq.csv 
 
 wc -l term_usage/"$ELEMENT"_fq.csv >> term_usage/term_cnts.txt
 
