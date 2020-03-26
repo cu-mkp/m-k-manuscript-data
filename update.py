10054a7e2dec1ac52a76a8703857fe4c5bf2e3c7
matthewkumar <matthewravikumar@gmail.com>
2020-03-26 14:50:34 -0400

    all folios

--- update.py
+++ update.py
@@ -66,6 +66,28 @@ def update_entries(manuscript: BnF) -> None:
         f_xml.write(content_xml)
         f_xml.close()
 
+def update_all_folios(manuscript: BnF) -> None:
+  """
+  Update /m-k-manuscript-data/allFolios/ with the current manuscript from /ms-xml/. 
+
+  Input:
+    manuscript -- Python object of the manuscript defined in digital_manuscript.py
+  Output:
+    None
+  """
+  for b in [True, False]:
+    for version in versions:
+      text = ''
+      folder = 'xml' if b else 'txt'
+
+      for identity, entry in manuscript.entries.items():
+        new_text = entry.text(version, xml=b)
+        text = f'{text}\n\n{new_text}' if text else new_text
+
+      f = open(f'{m_path}/allFolios/{folder}/all_{version}.{folder}', 'w')
+      f.write(text)
+      f.close()
+
 def update_time():
   """ Extract timestamp at the top of this file and update it. """
   # Initialize date to write and container for the text
