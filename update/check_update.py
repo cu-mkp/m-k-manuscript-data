b3cb0166e15e93676ee89d666d4ea3f90396cbbb
matthewkumar <matthewravikumar@gmail.com>
2020-05-25 16:40:25 -0400

    debugging in check update

--- update/check_update.py
+++ update/check_update.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 
 def check_update():
+  print('running update')
+
   now = datetime.strptime(str(datetime.now()).split(' ')[0], '%Y-%m-%d')
   with open('./update/update.py', 'r') as f:
     text = f.read()
     comment = text.split('\n')[0]
     timestamp = datetime.strptime(comment.split('|')[1].strip(), '%Y-%m-%d')
+
+    print(timestamp, now)
 
     if timestamp < now:
       print('The repository has not been updated since ' + str(timestamp) + '. Please run update.py on the day of merging the branch.')
