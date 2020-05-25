524c08a57483223f29bc89d5a1a05eaf87645fbb
matthewkumar <matthewravikumar@gmail.com>
2020-05-25 15:47:50 -0400

    shell script directly in .travis.yml

--- check_update.py
+++ check_update.py
@@ -1,11 +1,8 @@
 # Last Updated | 2019-11-10
 
-import os
 from datetime import datetime
 
-import pandas as pd
-
-def check_time():
+def check_update():
   now = datetime.strptime(str(datetime.now()).split(' ')[0], '%Y-%m-%d')
   with open('./update.py', 'r') as f:
     text = f.read()
@@ -15,11 +12,5 @@ def check_time():
     if timestamp < now:
       print('The repository has not been updated since ' + str(timestamp) + '. Please run update.py on the day of merging the branch.')
       assert False
-
-  print('The branch has been updated recently')
-
-
-def check_update():
-  check_time()
 
 check_update()
--- check_update.sh
+++ check_update.sh
@@ -1,12 +1,7 @@
-CHANGED_FILES=
-
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] ; then
     # This isn't a PR build.
-    COMMIT_RANGE="$(echo ${TRAVIS_COMMIT_RANGE} | cut -d '.' -f 1,4 --output-delimiter '..')"
-    CHANGED_FILES="$(git diff --name-only ${COMMIT_RANGE} --)"
+    true
 else
     # This is a PR build.
-    CHANGED_FILES="$(git diff --name-only ${TRAVIS_BRANCH}..HEAD --)"
     python check_update.py
 fi
-echo "End Test"
