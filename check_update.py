411606b557f07878d574cab91f9ae800ba71b8fe
matthewkumar <matthewravikumar@gmail.com>
2020-05-25 15:32:38 -0400

    temporarily removing validation; call python with shell script

--- check_update.py
+++ check_update.py
@@ -16,55 +16,8 @@ def check_time():
       print('The repository has not been updated since ' + str(timestamp) + '. Please run update.py on the day of merging the branch.')
       assert False
 
-def check_metadata():
-  dummy_metadata = './dummy/entry_metadata.csv'
-  real_metadata = './metadata/entry_metadata.csv'
+  print('The branch has been updated recently')
 
-  dummy_df = pd.read_csv(dummy_metadata)
-  dummy_df.fillna('', inplace=True)
-  real_df = pd.read_csv(real_metadata)
-  real_df.fillna('', inplace=True)
-
-  if dummy_df.shape != real_df.shape:
-    return False
-
-  dummy_cols = dummy_df.columns
-  real_cols = real_df.columns
-
-  for i, col in enumerate(dummy_cols):
-    if col != real_cols[i]:
-      return False
-  
-  for i, row in dummy_df.iterrows():
-    dummy_row = dummy_df.loc[i, : ]
-    real_row = real_df.loc[i, : ]
-
-    for j, col in enumerate(dummy_cols):
-      dummy_cell = set(list(dummy_row[col]))
-      real_cell = set(list(real_row[col]))
-
-      if dummy_cell != real_cell:        
-        return False
-
-  return True
-
-def check_all_folios():
-  for folder in ['xml', 'txt']:
-    for version in ['tc', 'tcn', 'tl']:
-      real_file = f'./allFolios/{folder}/all_{version}.{folder}'
-      dummy_file = f'./dummy/all_{version}.{folder}'
-
-      real_text = ''
-      with open(real_file) as rf:
-        real_text = rf.read()
-
-      dummy_text = ''
-      with open(dummy_file) as df:
-        dummy_text = df.read()
-
-      if real_text != dummy_text:
-        return False
-  return True
 
 def check_update():
   check_time()
--- check_update.sh
+++ check_update.sh
@@ -4,10 +4,9 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] ; then
     # This isn't a PR build.
     COMMIT_RANGE="$(echo ${TRAVIS_COMMIT_RANGE} | cut -d '.' -f 1,4 --output-delimiter '..')"
     CHANGED_FILES="$(git diff --name-only ${COMMIT_RANGE} --)"
-    echo "Not a PR"
 else
     # This is a PR build.
     CHANGED_FILES="$(git diff --name-only ${TRAVIS_BRANCH}..HEAD --)"
-    echo "This is a PR"
+    python check_update.py
 fi
 echo "End Test"
